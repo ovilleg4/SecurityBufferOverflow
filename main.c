36eb6de53d0177f9e3cf7c48a42e232f61c7ddf6
ovileg4 <ovilleg4@gmail.com>
2017-05-11 20:43:04 -0600

    Returning to printing without pointer

--- main.c
+++ main.c
@@ -33,6 +33,7 @@ int main(int argc, char** argv) {
     long *ptr = (long *) buffer;
     printf("buffer address: %p\n", (void *) &buffer);
     printf("buffer size: bytes: %d\n", (void *) &length - (void *) &buffer);
+    printf("buffer size using size of %d", sizeof(buffer));
     //ptr = (long *) userid;
     //printf("userid address: %p\n", (void *) &userid);
     //ptr = (long *) targetuid;
